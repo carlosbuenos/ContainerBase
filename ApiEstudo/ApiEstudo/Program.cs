b9d67dda88d409b354450b80370cf20dd22c11ed
carlos roberto cardoso dos reis bueno <ccarlosbueno@outlook.com>
2019-12-17 21:25:06 -0300

    80 port

--- ApiEstudo/ApiEstudo/Program.cs
+++ ApiEstudo/ApiEstudo/Program.cs
@@ -28,7 +28,7 @@ namespace ApiEstudo
 				{
 					webBuilder.UseStartup<Startup>().UseKestrel(options =>
 					{
-						options.Listen(IPAddress.Any, 83);
+						options.Listen(IPAddress.Any, 80);
 					});
 				});
 	}
