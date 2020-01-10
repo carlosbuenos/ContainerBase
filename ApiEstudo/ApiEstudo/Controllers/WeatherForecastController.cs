2101627de6567d77012d32b34357660d0a1cd734
carlos roberto cardoso dos reis bueno <ccarlosbueno@outlook.com>
2020-01-10 10:34:55 -0300

    small fix

--- ApiEstudo/ApiEstudo/Controllers/WeatherForecastController.cs
+++ ApiEstudo/ApiEstudo/Controllers/WeatherForecastController.cs
@@ -37,6 +37,8 @@ namespace ApiEstudo.Controllers
 		[HttpGet]
 		public IEnumerable<WeatherForecast> Get()
 		{
+
+			//Testei
 			var rng = new Random();
 			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
 			{
